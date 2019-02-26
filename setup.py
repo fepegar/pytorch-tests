e2174f7d77af3f600d71a574360c7a90dae31403
Fernando Perez-Garcia <fepegar@gmail.com>
2019-02-26 09:44:55 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Conda requires: conda install pytorch torchvision -c pytorch
+
+setup(
+    name='pytorch_tests',
+    version='0.1.0',
+    author='Fernando Perez-Garcia',
+    author_email='fernando.perezgarcia.17@ucl.ac.uk',
+    packages=find_packages(exclude=['*tests']),
+    install_requires=[
+        'namedtensor',
+        'tensorflow',
+        'tensorboardX',
+        'matplotlib',
+        'tqdm',
+    ],
+)
